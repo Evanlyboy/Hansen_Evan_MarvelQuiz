cf4e65f9f880bc80644dc764ebba141499854587
Evan <39568306+Evanlyboy@users.noreply.github.com>
2021-12-08 18:07:12 -0500

    I think it's almost done, I set up the functions to check the values and now I'm hoping it works

--- gameComponents/characters.py
+++ gameComponents/characters.py
@@ -20,4 +20,3 @@ carnage = 0
 characters = ["spider-man", "cyclops", "mysterio", "carnage"]
 
 question = ["How did your character's power manifest? Latent or Acquired: ", "What is your character's temperament: Calm or Erratic?", "Does your character have a good social life? Yes or No :", "What does your character fight for? Good or Bad:"]
-		
